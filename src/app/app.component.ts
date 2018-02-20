1192381f3d12a65665c3dffa8a0c0a105663263d
Can Kattw <kattwinkel@w11k.de>
2018-02-20 10:39:39 +0100

    starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BlogService} from './common/blog/blog.service';
 import {Observable} from 'rxjs/Observable';
 import {FormControl, FormGroup} from '@angular/forms';
-import {Store} from '@ngrx/store';
-import {getCombinedPosts, getPostSearchForm, getPostSearchIsFetching, RootState} from './app.store';
 import {SearchForm} from './search-form.model';
-import * as postSearchActions from './post-search/post-search.actions';
-import {FormUpdate} from './post-search/post-search.actions';
 import {Post, PostWithComments} from './common/blog/post/post.model';
-import {startWith, takeUntil} from 'rxjs/operators';
-import {componentDestroyed} from 'ng2-rx-componentdestroyed';
 
 @Component({
   selector: 'rvt-root',
@@ -21,11 +15,9 @@ export class AppComponent implements OnInit, OnDestroy {
   posts$: Observable<PostWithComments[] | Post[]>;
   searchForm: FormGroup;
 
-  constructor(private readonly blogService: BlogService, private store: Store<RootState>) {
+  constructor(private readonly blogService: BlogService) {
     this.initForm();
 
-    this.loading$ = this.store.select(getPostSearchIsFetching);
-    this.posts$ = this.store.select(getCombinedPosts);
   }
 
 
@@ -35,26 +27,13 @@ export class AppComponent implements OnInit, OnDestroy {
       includeComments: new FormControl(),
     });
 
-    this.searchForm.valueChanges
-      .pipe(
-        takeUntil(componentDestroyed(this)),
-        startWith(this.searchForm.value)
-      )
-      .subscribe((updates: SearchForm) => this.store.dispatch(new FormUpdate(updates)));
   }
 
   ngOnInit() {
-    this.store.select(getPostSearchForm)
-      .pipe(takeUntil(componentDestroyed(this)))
-      .subscribe(updates => this.updateForm(updates));
   }
 
   updateForm(patch: Partial<SearchForm>) {
     this.searchForm.patchValue(patch, {emitEvent: false, onlySelf: false});
-  }
-
-  go() {
-    this.store.dispatch(new postSearchActions.FetchPosts(this.searchForm.value));
   }
 
   ngOnDestroy(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BlogModule} from './common/blog/blog.module';
-import {StoreModule} from '@ngrx/store';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '../environments/environment';
-import {EffectsModule} from '@ngrx/effects';
-import {AppEffects} from './app.effects';
 import {ReactiveFormsModule} from '@angular/forms';
-import {reducers} from './app.store';
 
 
 @NgModule({
@@ -19,9 +13,6 @@ import {reducers} from './app.store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(reducers),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    EffectsModule.forRoot([AppEffects]),
     BlogModule,
     ReactiveFormsModule,
   ],
